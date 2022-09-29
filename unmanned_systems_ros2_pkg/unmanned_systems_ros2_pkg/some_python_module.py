df1565598b73d9c38cb300a4ab96f900121b13e3
jn89b <jnguyenblue2804@gmail.com>
2022-09-28 20:04:37 -0500

    Revert to 56e05fce

--- unmanned_systems_ros2_pkg/unmanned_systems_ros2_pkg/some_python_module.py
+++ unmanned_systems_ros2_pkg/unmanned_systems_ros2_pkg/some_python_module.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-from rclpy.node import Node
-
-def get_time_in_secs(some_node:Node) -> float:
-    return some_node.get_clock().now().nanoseconds /1E9
 
 def print_hello():
         print("Import function HELLO WORLD")
